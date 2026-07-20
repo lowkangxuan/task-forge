c9616b4736bdfce0c6295a147bef1c1410f2ce36
kang <lowkangxuan@gmail.com>
2026-07-20 15:31:28 +0800

    feat: updated project actions UI block

--- src/features/project/components/project-actions.tsx
+++ src/features/project/components/project-actions.tsx
@@ -1,7 +1,19 @@
 import { Button } from "@/components/ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { Ellipsis } from "lucide-react";
+import { Copy, Ellipsis, Trash } from "lucide-react";
 
+const actions = [
+    [
+        {
+            label: "Duplicate",
+            icon: Copy,
+        },
+        {
+            label: "Delete",
+            icon: Trash,
+        },
+    ]
+]
 
 export function ProjectActions() {
     return (
@@ -11,8 +23,18 @@ export function ProjectActions() {
                     <Ellipsis />
                 </Button>
             } />
-            <PopoverContent align="end">
-                Testing
+            <PopoverContent align="end" className="w-56 overflow-hidden rounded-lg p-0">
+                <div className="flex flex-col">
+                    {actions.map((section, index) => (
+                        <div key={index} className="flex flex-col p-2 gap-1 border-b last:border-none">
+                            {section.map((item, index) => (
+                                <Button key={index} variant="ghost" className="justify-start text-sm font-normal gap-2">
+                                    <item.icon /> <span>{item.label}</span>
+                                </Button>
+                            ))}
+                        </div>
+                    ))}
+                </div>
             </PopoverContent>
         </Popover>
     )
--- src/server/projects.ts
+++ src/server/projects.ts
@@ -16,6 +16,18 @@ export const verifyProjectOwnership = createServerFn({ method: "GET" })
         return validProject.length > 0;
     })
 
+export const createNewProject = createServerFn({ method: "POST" })
+    .middleware([authMiddleware])
+    .inputValidator((data: { name: string, description: string }) => data)
+    .handler(async ({ data, context }) => {
+        await db.insert(projects).values(
+            {
+                userId: context.user.id,
+                name: data.name,
+                description: data.description,
+            });
+    });
+
 export const getUserProjects = createServerFn({ method: "GET" })
     .middleware([authMiddleware])
     .handler(async ({ context }) => {
@@ -31,17 +43,6 @@ export const getUserProjects = createServerFn({ method: "GET" })
         return userProjects;
     });
 
-export const createNewProject = createServerFn({ method: "POST" })
-    .middleware([authMiddleware])
-    .inputValidator((data: { name: string, description: string }) => data)
-    .handler(async ({ data, context }) => {
-        await db.insert(projects).values(
-            {
-                userId: context.user.id,
-                name: data.name,
-                description: data.description,
-            });
-    });
 
 export const getProject = createServerFn({ method: "GET" })
     .middleware([authMiddleware])
@@ -64,4 +65,18 @@ export const updateProjectName = createServerFn({ method: "POST" })
     .inputValidator((data: { projectId: string, name: string }) => data)
     .handler(async ({ data }) => {
         await db.update(projects).set({ name: data.name }).where(eq(projects.id, data.projectId));
+    })
+
+export const deleteProject = createServerFn({ method: "POST" })
+    .middleware([authMiddleware])
+    .inputValidator(z.object({
+        projectId: z.string()
+    }))
+    .handler(async ({ data, context }) => {
+        await db.delete(projects).where(
+            and(
+                eq(projects.id, data.projectId),
+                eq(projects.userId, context.user.id),
+            )
+        )
     })
